594e2fe97e78754b9b2850bb8a5c36ed6707fb67
Liam White <181906045+bettererliamw@users.noreply.github.com>
2026-06-30 09:04:47 -0400

    Update Blink_Exploration.py
    Fixed a typo in Line 8 comment, Added extra info to Line 11 comment

--- 01_first_examples/code/exploration_cheat_sheet/Blink_Exploration.py
+++ 01_first_examples/code/exploration_cheat_sheet/Blink_Exploration.py
@@ -5,6 +5,6 @@ led=Pin(2,Pin.OUT) #create LED object from pin2,Set Pin2 to output
 
 while True:
     led.value(1)    # Turn LED On
-    sleep_ms(100)   # Can you make the LED blink fast it looks like its always on?
+    sleep_ms(100)   # Can you make the LED blink fast enough that it looks like it's always on?
     led.value(0)    # Turn LED Off
-    sleep_ms(35)    # You can also try giving them different on/off times!
+    sleep_ms(35)    # You can also try giving them different on/off times by changing the number inside the Parentheses ()!
