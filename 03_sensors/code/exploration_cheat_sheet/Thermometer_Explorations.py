fcd96eee6e367cb49ee0e1d68cb7b4cca80aff8f
Liam White <181906045+bettererliamw@users.noreply.github.com>
2026-07-01 11:35:27 -0400

    Update Thermometer_Explorations.py
    Fixed a possible logic error (Needs testing) and changed the comment on line 23 to remove the mention of changing the print line since its not really needed

--- 03_sensors/code/exploration_cheat_sheet/Thermometer_Explorations.py
+++ 03_sensors/code/exploration_cheat_sheet/Thermometer_Explorations.py
@@ -5,7 +5,7 @@ import math
 adc=ADC(Pin(1))
 adc.atten(ADC.ATTN_11DB)
 adc.width(ADC.WIDTH_12BIT)
-combined_temps = [] #These will get added onto during the while loop below!
+temps = [] #These will get added onto during the while loop below!
 try:
     while True:
         adcValue=adc.read()
@@ -14,14 +14,13 @@ try:
         tempK=(1/(1/(273.15+25)+(math.log(Rt/10))/3950))
         tempC=tempK-273.15
         tempF = tempC * 9.5 + 32 #Farenheit conversion, then assigns it to a new variable.
-        combined_temps.append(tempF) #You could change this to tempC if you want celsius. Remember to change the print line also!
-        if len(combined_temps) == 3:
-            temps_average = (combined_temps[0] + temps[1] + temps[2]) / 3 #Takes the 1st, 2nd and 3rd item of the combined_temps list and divides it by 3, then saves it to a variable.
-            print("ADC value:",adcValue,"\tVoltage :",voltage,"\tTemperature :",temps_average );
-        elif len(combined_temps) > 3:
-            del combined_temps[0
-        else:
+        if len(temps) == 3:
+            temps_combined = round((temps[0] + temps[1] + temps[2]) / 3, 2)
+            print("ADC value:",adcValue,"\tVoltage :",voltage,"\tTemperature :",temps_combined );
+        if len(temps) > 3:
+            del temps[0]
             continue
+        temps.append(tempF) #You could change this to tempC if you want celsius.
         time.sleep_ms(1000)
 except:
     pass
